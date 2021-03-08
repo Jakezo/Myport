b466e1c02c481bef78df6a54ecec295d1074d8c4
Jakezo <cakim21@naver.com>
2021-03-08 14:55:55 +0900

    Updates

---
+++ precache-manifest.9b79e3dd26f2c1f5b41f9e83c35403d8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc91783e34e9778b0c07",
+    "url": "/myport/static/js/main.bf62d7f1.chunk.js"
+  },
+  {
+    "revision": "2051b44b4e1d9ff5bd19",
+    "url": "/myport/static/js/runtime~main.84d4e2c8.js"
+  },
+  {
+    "revision": "0e22ec92ffa5dbb54790",
+    "url": "/myport/static/js/2.935a34b0.chunk.js"
+  },
+  {
+    "revision": "541058d6d91825ac973653dd14203a67",
+    "url": "/myport/static/media/Home.541058d6.png"
+  },
+  {
+    "revision": "79f689ee44597476bf4c7b13de8ce538",
+    "url": "/myport/static/media/Profile.79f689ee.png"
+  },
+  {
+    "revision": "7d352db8732233accb83ae3297bed816",
+    "url": "/myport/static/media/jquery.7d352db8.png"
+  },
+  {
+    "revision": "ce2716867f035b952813b224bffa915d",
+    "url": "/myport/static/media/mev.ce271686.png"
+  },
+  {
+    "revision": "ef000e5539a4737d85791899f7e58276",
+    "url": "/myport/static/media/portfolio.ef000e55.png"
+  },
+  {
+    "revision": "b436f8553cea3c9670c9db263306e46c",
+    "url": "/myport/index.html"
+  }
+];
